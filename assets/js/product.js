23f9eb3a2d57ab148f02fab067c18ed250f3cb8c
EngagedMK <ngkhanh245@gmail.com>
2025-06-06 09:44:16 +0700

    fix: fix product #07

--- assets/js/product.js
+++ assets/js/product.js
@@ -109,169 +109,172 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 });
 
+// document.addEventListener("DOMContentLoaded", () => {
+//   const productImgWrapper = document.querySelector(".product__img-wrapper");
+//   const productImgCarousel = document.querySelector(".product__img-carousel");
+//   if (!productImgCarousel) {
+//     console.warn("Product image carousel not found!");
+//     return;
+//   }
+
+//   const firstCard = productImgCarousel.querySelector(".product__img-card");
+//   const firstCardWidthImg = firstCard ? firstCard.offsetWidth : 0;
+//   const arrowBtnsImg = document.querySelectorAll(".product__img-wrapper i");
+//   const productImgCarouselChildrens = [...(productImgCarousel?.children || [])];
+//   // const paginationContainer = document.querySelector(
+//   //   ".product__img-pagination"
+//   // );
+//   let isDraggingImg = false,
+//     isAutoPlayImg = true,
+//     startImgX,
+//     startScrollLeftImg,
+//     timeoutImgId,
+//     currentIndexImg = 0;
+
+//   let cardPerViewImg = firstCardWidthImg
+//     ? Math.round(productImgCarousel.offsetWidth / firstCardWidthImg)
+//     : 1;
+
+//   productImgCarouselChildrens
+//     .slice(-cardPerViewImg)
+//     .reverse()
+//     .forEach((card) => {
+//       productImgCarousel.insertAdjacentHTML("afterbegin", card.outerHTML);
+//     });
+
+//   productImgCarouselChildrens.slice(0, cardPerViewImg).forEach((card) => {
+//     productImgCarousel.insertAdjacentHTML("beforeend", card.outerHTML);
+//   });
+
+//   productImgCarousel.classList.add("no-transition");
+//   productImgCarousel.scrollLeft = productImgCarousel.offsetWidth;
+//   productImgCarousel.classList.remove("no-transition");
+
+//   const originalSlidesImg = 7;
+// if (!paginationContainer) {
+//   console.warn("Pagination container (.product__img-pagination) not found!");
+//   return;
+// }
+// for (let i = 0; i < originalSlidesImg; i++) {
+//   const bullet = document.createElement("span");
+//   bullet.classList.add("product__img-pagination-bullet");
+//   if (i === 0) bullet.classList.add("active");
+//   bullet.addEventListener("click", () => goToSlideImg(i));
+//   paginationContainer.appendChild(bullet);
+// }
+
+// function goToSlideImg(index) {
+//   currentIndexImg = index;
+//   const scrollTo = index * firstCardWidthImg + productImgCarousel.offsetWidth;
+//   productImgCarousel.scrollLeft = scrollTo;
+//   updatePaginationImg();
+// }
+
+// function updatePaginationImg() {
+//   const bullets = paginationContainer.querySelectorAll(
+//     ".product__img-pagination-bullet"
+//   );
+//   bullets.forEach((bullet, index) => {
+//     bullet.classList.toggle("active", index === currentIndexImg);
+//   });
+// }
+
+// arrowBtnsImg.forEach((btn) => {
+//   btn.addEventListener("click", () => {
+//     productImgCarousel.scrollLeft +=
+//       btn.id === "left" ? -firstCardWidthImg : firstCardWidthImg;
+//     currentIndexImg =
+//       Math.round(productImgCarousel.scrollLeft / firstCardWidthImg) %
+//       originalSlidesImg;
+//     if (currentIndexImg < 0) currentIndexImg += originalSlidesImg;
+//     updatePaginationImg();
+//   });
+// });
+
+// const dragStartImg = (e) => {
+//   isDraggingImg = true;
+//   productImgCarousel.classList.add("dragging");
+//   startImgX = e.pageX || e.touches[0].pageX;
+//   startScrollLeftImg = productImgCarousel.scrollLeft;
+// };
+
+// const draggingImg = (e) => {
+//   if (!isDraggingImg) return;
+//   e.preventDefault();
+//   const x = e.pageX || e.touches[0].pageX;
+//   productImgCarousel.scrollLeft = startScrollLeftImg - (x - startImgX);
+// };
+
+// const dragStopImg = () => {
+//   isDraggingImg = false;
+//   productImgCarousel.classList.remove("dragging");
+//   currentIndexImg =
+//     Math.round(productImgCarousel.scrollLeft / firstCardWidthImg) %
+//     originalSlidesImg;
+//   if (currentIndexImg < 0) currentIndexImg += originalSlidesImg;
+//   updatePaginationImg();
+// };
+
+// const infiniteScrollImg = () => {
+//   if (productImgCarousel.scrollLeft === 0) {
+//     productImgCarousel.classList.add("no-transition");
+//     productImgCarousel.scrollLeft =
+//       productImgCarousel.scrollWidth - 2 * productImgCarousel.offsetWidth;
+//     productImgCarousel.classList.remove("no-transition");
+//     currentIndexImg = originalSlidesImg - 1;
+//   } else if (
+//     Math.ceil(productImgCarousel.scrollLeft) ===
+//     productImgCarousel.scrollWidth - productImgCarousel.offsetWidth
+//   ) {
+//     productImgCarousel.classList.add("no-transition");
+//     productImgCarousel.scrollLeft = productImgCarousel.offsetWidth;
+//     productImgCarousel.classList.remove("no-transition");
+//     currentIndexImg = 0;
+//   }
+//   updatePaginationImg();
+//   clearTimeout(timeoutImgId);
+//   if (!productImgWrapper.matches(":hover")) autoPlayImg();
+// };
+
+// const autoPlayImg = () => {
+//   if (window.innerWidth < 800 || !isAutoPlayImg) return;
+//   timeoutImgId = setTimeout(() => {
+//     productImgCarousel.scrollLeft += firstCardWidthImg;
+//     currentIndexImg = (currentIndexImg + 1) % originalSlidesImg;
+//     updatePaginationImg();
+//     autoPlayImg();
+//   }, 2500);
+// };
+
+// autoPlayImg();
+
+//   productImgCarousel.addEventListener("mousedown", dragStartImg);
+//   productImgCarousel.addEventListener("mousemove", draggingImg);
+//   document.addEventListener("mouseup", dragStopImg);
+//   productImgCarousel.addEventListener("scroll", infiniteScrollImg);
+//   productImgWrapper.addEventListener("mouseenter", () =>
+//     clearTimeout(timeoutImgId)
+//   );
+//   productImgWrapper.addEventListener("mouseleave", autoPlayImg);
+
+//   productImgCarousel.addEventListener("touchstart", dragStartImg);
+//   productImgCarousel.addEventListener("touchmove", draggingImg);
+//   productImgCarousel.addEventListener("touchend", dragStopImg);
+
+//   window.addEventListener("load", () => {
+//     const images = document.querySelectorAll(".product__img-img img");
+//     let maxHeight = 0;
+
+//     images.forEach((img) => {
+//       if (img.height > maxHeight) maxHeight = img.height;
+//     });
+
+//     productImgCarousel.style.minHeight = `${maxHeight}px`;
+//   });
+// });
+
 document.addEventListener("DOMContentLoaded", () => {
-  const productImgWrapper = document.querySelector(".product__img-wrapper");
-  const productImgCarousel = document.querySelector(".product__img-carousel");
-  if (!productImgCarousel) {
-    console.warn("Product image carousel not found!");
-    return;
-  }
-
-  const firstCard = productImgCarousel.querySelector(".product__img-card");
-  const firstCardWidthImg = firstCard ? firstCard.offsetWidth : 0;
-  const arrowBtnsImg = document.querySelectorAll(".product__img-wrapper i");
-  const productImgCarouselChildrens = [...(productImgCarousel?.children || [])];
-  const paginationContainer = document.querySelector(
-    ".product__img-pagination"
-  );
-  let isDraggingImg = false,
-    isAutoPlayImg = true,
-    startImgX,
-    startScrollLeftImg,
-    timeoutImgId,
-    currentIndexImg = 0;
-
-  let cardPerViewImg = firstCardWidthImg
-    ? Math.round(productImgCarousel.offsetWidth / firstCardWidthImg)
-    : 1;
-
-  productImgCarouselChildrens
-    .slice(-cardPerViewImg)
-    .reverse()
-    .forEach((card) => {
-      productImgCarousel.insertAdjacentHTML("afterbegin", card.outerHTML);
-    });
-
-  productImgCarouselChildrens.slice(0, cardPerViewImg).forEach((card) => {
-    productImgCarousel.insertAdjacentHTML("beforeend", card.outerHTML);
-  });
-
-  productImgCarousel.classList.add("no-transition");
-  productImgCarousel.scrollLeft = productImgCarousel.offsetWidth;
-  productImgCarousel.classList.remove("no-transition");
-
-  // [ADD 4] Add pagination bullets
-  const originalSlidesImg = 7; // Number of original slides
-  for (let i = 0; i < originalSlidesImg; i++) {
-    const bullet = document.createElement("span");
-    bullet.classList.add("product__img-pagination-bullet");
-    if (i === 0) bullet.classList.add("active");
-    bullet.addEventListener("click", () => goToSlideImg(i));
-    paginationContainer.appendChild(bullet);
-  }
-
-  function goToSlideImg(index) {
-    currentIndexImg = index;
-    const scrollTo = index * firstCardWidthImg + productImgCarousel.offsetWidth;
-    productImgCarousel.scrollLeft = scrollTo;
-    updatePaginationImg();
-  }
-
-  function updatePaginationImg() {
-    const bullets = paginationContainer.querySelectorAll(
-      ".product__img-pagination-bullet"
-    );
-    bullets.forEach((bullet, index) => {
-      bullet.classList.toggle("active", index === currentIndexImg);
-    });
-  }
-
-  arrowBtnsImg.forEach((btn) => {
-    btn.addEventListener("click", () => {
-      productImgCarousel.scrollLeft +=
-        btn.id === "left" ? -firstCardWidthImg : firstCardWidthImg;
-      currentIndexImg =
-        Math.round(productImgCarousel.scrollLeft / firstCardWidthImg) %
-        originalSlidesImg;
-      if (currentIndexImg < 0) currentIndexImg += originalSlidesImg;
-      updatePaginationImg();
-    });
-  });
-
-  const dragStartImg = (e) => {
-    isDraggingImg = true;
-    productImgCarousel.classList.add("dragging");
-    startImgX = e.pageX || e.touches[0].pageX;
-    startScrollLeftImg = productImgCarousel.scrollLeft;
-  };
-
-  const draggingImg = (e) => {
-    if (!isDraggingImg) return;
-    e.preventDefault();
-    const x = e.pageX || e.touches[0].pageX;
-    productImgCarousel.scrollLeft = startScrollLeftImg - (x - startImgX);
-  };
-
-  const dragStopImg = () => {
-    isDraggingImg = false;
-    productImgCarousel.classList.remove("dragging");
-    currentIndexImg =
-      Math.round(productImgCarousel.scrollLeft / firstCardWidthImg) %
-      originalSlidesImg;
-    if (currentIndexImg < 0) currentIndexImg += originalSlidesImg;
-    updatePaginationImg();
-  };
-
-  const infiniteScrollImg = () => {
-    if (productImgCarousel.scrollLeft === 0) {
-      productImgCarousel.classList.add("no-transition");
-      productImgCarousel.scrollLeft =
-        productImgCarousel.scrollWidth - 2 * productImgCarousel.offsetWidth;
-      productImgCarousel.classList.remove("no-transition");
-      currentIndexImg = originalSlidesImg - 1;
-    } else if (
-      Math.ceil(productImgCarousel.scrollLeft) ===
-      productImgCarousel.scrollWidth - productImgCarousel.offsetWidth
-    ) {
-      productImgCarousel.classList.add("no-transition");
-      productImgCarousel.scrollLeft = productImgCarousel.offsetWidth;
-      productImgCarousel.classList.remove("no-transition");
-      currentIndexImg = 0;
-    }
-    updatePaginationImg();
-    clearTimeout(timeoutImgId);
-    if (!productImgWrapper.matches(":hover")) autoPlayImg();
-  };
-
-  const autoPlayImg = () => {
-    if (window.innerWidth < 800 || !isAutoPlayImg) return;
-    timeoutImgId = setTimeout(() => {
-      productImgCarousel.scrollLeft += firstCardWidthImg;
-      currentIndexImg = (currentIndexImg + 1) % originalSlidesImg;
-      updatePaginationImg();
-      autoPlayImg();
-    }, 2500);
-  };
-
-  autoPlayImg();
-
-  productImgCarousel.addEventListener("mousedown", dragStartImg);
-  productImgCarousel.addEventListener("mousemove", draggingImg);
-  document.addEventListener("mouseup", dragStopImg);
-  productImgCarousel.addEventListener("scroll", infiniteScrollImg);
-  productImgWrapper.addEventListener("mouseenter", () =>
-    clearTimeout(timeoutImgId)
-  );
-  productImgWrapper.addEventListener("mouseleave", autoPlayImg);
-
-  productImgCarousel.addEventListener("touchstart", dragStartImg);
-  productImgCarousel.addEventListener("touchmove", draggingImg);
-  productImgCarousel.addEventListener("touchend", dragStopImg);
-
-  window.addEventListener("load", () => {
-    const images = document.querySelectorAll(".product__img-img img");
-    let maxHeight = 0;
-
-    images.forEach((img) => {
-      if (img.height > maxHeight) maxHeight = img.height;
-    });
-
-    productImgCarousel.style.minHeight = `${maxHeight}px`;
-  });
-});
-
-document.addEventListener("DOMContentLoaded", () => {
-  console.log("DOM fully loaded, starting script...");
+  // console.log("DOM fully loaded, starting script...");
 
   const productImgSmaillWrapper = document.querySelector(
     ".product__img-smaill-wrapper"
@@ -283,41 +286,54 @@ document.addEventListener("DOMContentLoaded", () => {
     ".product__img-smaill-pagination"
   );
 
-  console.log("Wrapper:", productImgSmaillWrapper);
-  console.log("Carousel:", productImgSmaillCarousel);
-  console.log("Pagination:", paginationSmaillContainer);
-
-  if (!productImgSmaillCarousel || !paginationSmaillContainer) {
-    console.warn("Carousel or pagination element not found!");
+  if (
+    !productImgSmaillCarousel ||
+    !paginationSmaillContainer ||
+    !productImgSmaillWrapper
+  ) {
+    console.warn("Carousel, pagination, or wrapper element not found!");
     return;
   }
 
-  console.log("All elements found, proceeding...");
+  // console.log("All elements found, proceeding...");
 
+  let firstCardWidthSmaill = 0;
   const firstSmaillCard = productImgSmaillCarousel.querySelector(
     ".product__img-smaill-card"
   );
-  const firstCardWidthSmaill = firstSmaillCard?.offsetWidth || 100;
-  let isDraggingSmaill = false,
-    isAutoPlaySmaill = true,
-    startXSmaill,
-    startScrollLeftSmaill,
-    currentIndexSmaill = 0;
-
   const originalSlidesSmaill = 3;
-  const totalPagesSmaill = originalSlidesSmaill;
+  const totalPagesSmaill = Math.round(originalSlidesSmaill / 2);
+  let isDraggingSmaill = false;
+  let startXSmaill = 0;
+  let startScrollLeftSmaill = 0;
+  let currentIndexSmaill = 0;
+
+  // Tính toán chiều rộng card
+  const updateCardWidth = () => {
+    firstCardWidthSmaill = firstSmaillCard?.offsetWidth || 100;
+  };
+  updateCardWidth(); // Tính ban đầu
+
+  // Debounce cho sự kiện scroll
+  const debounce = (func, wait) => {
+    let timeout;
+    return (...args) => {
+      clearTimeout(timeout);
+      timeout = setTimeout(() => func(...args), wait);
+    };
+  };
 
   function createPaginationSmaill() {
-    console.log("Creating pagination...");
+    // console.log("Creating pagination...");
     paginationSmaillContainer.innerHTML = `
-                    <button class="pagination-btn" id="prev"><</button>
-                    <span class="pagination-current">1/${totalPagesSmaill}</span>
-                    <button class="pagination-btn" id="next">></button>
-                `;
-    console.log(
-      "Pagination content set to:",
-      paginationSmaillContainer.innerHTML
-    );
+      <button class="pagination-btn" id="prev"><i class="fas fa-chevron-left"></i></button>
+      <span class="pagination-current">1/${totalPagesSmaill}</span>
+      <button class="pagination-btn" id="next"><i class="fas fa-chevron-right"></i></button>
+    `;
+    // console.log(
+    //   "Pagination content set to:",
+    //   paginationSmaillContainer.innerHTML
+    // );
     updatePaginationSmaill();
   }
 
@@ -355,26 +371,26 @@ document.addEventListener("DOMContentLoaded", () => {
   const dragStartSmaill = (e) => {
     isDraggingSmaill = true;
     productImgSmaillCarousel.classList.add("product__img-smaill-dragging");
-    startXSmaill = e.pageX || e.touches[0].pageX;
+    startXSmaill = e.pageX || (e.touches && e.touches[0].pageX);
     startScrollLeftSmaill = productImgSmaillCarousel.scrollLeft;
   };
 
   const draggingSmaill = (e) => {
     if (!isDraggingSmaill) return;
     e.preventDefault();
-    const x = e.pageX || e.touches[0].pageX;
+    const x = e.pageX || (e.touches && e.touches[0].pageX);
     const newScrollLeft = startScrollLeftSmaill - (x - startXSmaill);
-    if (
-      newScrollLeft >= 0 &&
-      newScrollLeft <=
-        productImgSmaillCarousel.scrollWidth -
-          productImgSmaillCarousel.offsetWidth
-    ) {
+    const maxScrollLeft =
+      productImgSmaillCarousel.scrollWidth -
+      productImgSmaillCarousel.offsetWidth;
+
+    if (newScrollLeft >= 0 && newScrollLeft <= maxScrollLeft) {
       productImgSmaillCarousel.scrollLeft = newScrollLeft;
     }
   };
 
   const dragStopSmaill = () => {
+    if (!isDraggingSmaill) return;
     isDraggingSmaill = false;
     productImgSmaillCarousel.classList.remove("product__img-smaill-dragging");
     const nearestIndex = Math.round(
@@ -387,50 +403,32 @@ document.addEventListener("DOMContentLoaded", () => {
     goToSlideSmaill(currentIndexSmaill);
   };
 
-  const handleScrollSmaill = () => {
-    if (!isDraggingSmaill) {
-      const nearestIndex = Math.round(
-        productImgSmaillCarousel.scrollLeft / firstCardWidthSmaill
-      );
-      currentIndexSmaill = Math.max(
-        0,
-        Math.min(nearestIndex, totalPagesSmaill - 1)
-      );
-      updatePaginationSmaill();
-    }
-  };
-
-  const autoPlaySmaill = () => {
-    if (window.innerWidth < 800 || !isAutoPlaySmaill) return;
-    if (currentIndexSmaill < totalPagesSmaill - 1) {
-      currentIndexSmaill += 1;
-      goToSlideSmaill(currentIndexSmaill);
-    }
-    setTimeout(autoPlaySmaill, 2500);
-  };
-
+  const handleScrollSmaill = debounce(() => {
+    if (isDraggingSmaill) return;
+    const nearestIndex = Math.round(
+      productImgSmaillCarousel.scrollLeft / firstCardWidthSmaill
+    );
+    currentIndexSmaill = Math.max(
+      0,
+      Math.min(nearestIndex, totalPagesSmaill - 1)
+    );
+    updatePaginationSmaill();
+  }, 50);
   productImgSmaillCarousel.addEventListener("mousedown", dragStartSmaill);
   productImgSmaillCarousel.addEventListener("mousemove", draggingSmaill);
   document.addEventListener("mouseup", dragStopSmaill);
-  productImgSmaillCarousel.addEventListener("scroll", handleScrollSmaill);
-  productImgSmaillWrapper.addEventListener("mouseenter", () => {
-    isAutoPlaySmaill = false;
-  });
-  productImgSmaillWrapper.addEventListener("mouseleave", () => {
-    isAutoPlaySmaill = true;
-    autoPlaySmaill();
-  });
-
   productImgSmaillCarousel.addEventListener("touchstart", dragStartSmaill);
   productImgSmaillCarousel.addEventListener("touchmove", draggingSmaill);
   productImgSmaillCarousel.addEventListener("touchend", dragStopSmaill);
+  productImgSmaillCarousel.addEventListener("scroll", handleScrollSmaill);
 
+  // Xử lý nút phân trang
   paginationSmaillContainer.addEventListener("click", (e) => {
-    if (e.target.id === "prev" && currentIndexSmaill > 0) {
+    if (e.target.closest("#prev") && currentIndexSmaill > 0) {
       currentIndexSmaill -= 1;
       goToSlideSmaill(currentIndexSmaill);
     } else if (
-      e.target.id === "next" &&
+      e.target.closest("#next") &&
       currentIndexSmaill < totalPagesSmaill - 1
     ) {
       currentIndexSmaill += 1;
@@ -438,12 +436,15 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 
+  // Xử lý resize
   window.addEventListener("resize", () => {
+    updateCardWidth(); // Cập nhật chiều rộng card
     initCarouselSmaill();
+    goToSlideSmaill(currentIndexSmaill); // Điều chỉnh vị trí slide
     updatePaginationSmaill();
   });
 
+  // Khởi tạo
   createPaginationSmaill();
   initCarouselSmaill();
-  autoPlaySmaill();
 });
